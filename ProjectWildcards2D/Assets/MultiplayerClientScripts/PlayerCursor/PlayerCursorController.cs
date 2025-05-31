d9519840b5e11e522d50d0a8deeea0b0543ec5f7
Butlchr17 <butlchr17@gmail.com>
2025-05-31 04:44:04 -0700

    Added Touch-Drag support for Mobile Device and Tile Highlighting Logic
    - updated PlayerCursorController.cs

--- ProjectWildcards2D/Assets/MultiplayerClientScripts/PlayerCursor/PlayerCursorController.cs
+++ ProjectWildcards2D/Assets/MultiplayerClientScripts/PlayerCursor/PlayerCursorController.cs
@@ -9,92 +9,149 @@ namespace SavageCrown
     public class PlayerCursorController : MonoBehaviourPun
     {
 
-        #region Private Fields
+            #region Private Fields
 
-        [SerializeField]
-        private float cursorSpeed = 5f;
+            [SerializeField]
+            private float cursorSpeed = 5f;
 
-        [SerializeField]
-        private RectTransform boardBounds; // Reference to the host client's BoardCanvas RectTransform
+            [SerializeField]
+            private RectTransform boardBounds; // Reference to the host client's BoardCanvas RectTransform
 
-        [SerializeField]
-        private Camera mainCam;
+            [SerializeField]
+            private Camera mainCam;
 
-        #endregion
+            [SerializeField]
+            private bool canHighlightTiles = false;
 
-        #region MonoBehaviour Callbacks
+            private GameObject currentHighlightedTile;
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            if (mainCam == null)
+            #endregion
+
+            #region MonoBehaviour Callbacks
+
+            // Start is called before the first frame update
+            void Start()
             {
-                mainCam = Camera.main;
-            }
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            if (!photonView.IsMine)
-            {
-                return;
-            }
-
-            HandleTouchMovement();
-        }
-
-        #endregion
-
-        #region  Private Methods
-
-        private void HandleTouchMovement()
-        {
-
-#if UNITY_ANDROID || UNITY_IOS || UNITY_EDITOR
-            
-            if (Input.touchCount > 0){
-                Touch touch = Input.GetTouch(0);
-
-                if(touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+                if (mainCam == null)
                 {
-                    Vector3 touchPos = mainCam.ScreenToWorldPoint(touch.position);
-                    touchPos.z = 0f;
-
-                    //Clamp to Board
-                    Vector3 clampedPos = ClampToBoard(touchPos);
-                    transform.position = Vector3.Lerp(transform.position, clampedPos, cursorSpeed * Time.deltaTime);
+                    mainCam = Camera.main;
                 }
             }
 
-#endif
-        }
-
-
-        private Vector3 ClampToBoard(Vector3 targetPos)
-        {
-            if (boardBounds == null)
+            // Update is called once per frame
+            void Update()
             {
-                Debug.LogWarning("PlayerCursorController: BoardBounds not assigned!");
-                return targetPos;
+                if (!photonView.IsMine)
+                {
+                    return;
+                }
+
+                HandleTouchMovement();
+
+                if (canHighlightTiles && GameManager.Instance.IsPlayerTurn(photonView.OwnerActorNr))
+                {
+                    HighlightTileUnderCursor();
+                }
+                else
+                {
+                    ClearHighlight();
+                }
             }
 
-            Vector3[] corners = new Vector3[4];
-            boardBounds.GetWorldCorners(corners);
+            #endregion
 
-            float minX = corners[0].x;
-            float minY = corners[0].y;
-            float maxX = corners[2].x;
-            float maxY = corners[2].y;
+            #region  Private Methods
 
-            float clampedX = Mathf.Clamp(targetPos.x, minX, maxX);
-            float clampedY = Mathf.Clamp(targetPos.y, minY, maxY);
+            private void HandleTouchMovement()
+            {
 
-            return new Vector3(clampedX, clampedY, 0f);
+    #if UNITY_EDITOR
+                if (Input.GetMouseButton(0))
+                {
+                    Vector3 mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+                    mousePos.z = 0f;
+
+                    Vector3 clampedPos = ClampToBoard(mousePos);
+                    transform.position = Vector3.Lerp(transform.position, clampedPos, cursorSpeed * Time.deltaTime);
+                }
+    #endif
+
+
+    #if UNITY_ANDROID || UNITY_IOS
+                
+                if (Input.touchCount > 0){
+                    Touch touch = Input.GetTouch(0);
+
+                    if(touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+                    {
+                        Vector3 touchPos = mainCam.ScreenToWorldPoint(touch.position);
+                        touchPos.z = 0f;
+
+                        //Clamp to Board
+                        Vector3 clampedPos = ClampToBoard(touchPos);
+                        transform.position = Vector3.Lerp(transform.position, clampedPos, cursorSpeed * Time.deltaTime);
+                    }
+                }
+
+    #endif
+
+            private void HighlightTileUnderCursor()
+            {
+
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.zero);
+
+                if (hit.collider != null && hit.collider.CompareTag("Tile"))
+                {
+                    Transform glow = hit.collider.transform.Find("GlowOverlay");
+
+                    if (glow != null && glow.gameObject != currentHighlightedTile)
+                    {
+                        ClearHighlight(); // Turn off previous one
+
+                        glow.gameObject.SetActive(true);
+                        currentHighlightedTile = glow.gameObject;
+                    }
+                }
+                else
+                {
+                    ClearHighlight();
+                }
+            }
+
+            private void ClearHighlight()
+            {
+                if (currentHighlightedTile != null)
+                {
+                    currentHighlightedTile.SetActive(false);
+                    currentHighlightedTile = null;
+                }
+            }
+
+            private Vector3 ClampToBoard(Vector3 targetPos)
+            {
+                if (boardBounds == null)
+                {
+                    Debug.LogWarning("PlayerCursorController: BoardBounds not assigned!");
+                    return targetPos;
+                }
+
+                Vector3[] corners = new Vector3[4];
+                boardBounds.GetWorldCorners(corners);
+
+                float minX = corners[0].x;
+                float minY = corners[0].y;
+                float maxX = corners[2].x;
+                float maxY = corners[2].y;
+
+                float clampedX = Mathf.Clamp(targetPos.x, minX, maxX);
+                float clampedY = Mathf.Clamp(targetPos.y, minY, maxY);
+
+                return new Vector3(clampedX, clampedY, 0f);
+            }
+
+            #endregion
+
         }
-
-        #endregion
-
     }
 }
 
